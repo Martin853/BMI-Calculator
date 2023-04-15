b7e9a87adea0a7a6be342103606771ade3f12158
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-04-15 16:51:16 +0200

    Created the calculation logic

--- bmi-calculator/src/components/Form.jsx
+++ bmi-calculator/src/components/Form.jsx
@@ -1,14 +1,21 @@
 import classes from './Form.module.css';
 
 function Form() {
+  function submitHandler(event) {
+    event.preventDefault();
+    const weightInput = document.querySelector('#weight');
+    const heightInput = document.querySelector('#height');
+    console.log(weightInput.value / heightInput.value ** 2);
+  }
+
   return (
     <div className={classes.container}>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <label htmlFor="weight">Weight</label>
         <input type="number" id="weight" />
         <label htmlFor="height">Height</label>
         <input type="number" id="height" />
-        <button>Calculate</button>
+        <button type="submit">Calculate</button>
       </form>
     </div>
   );
